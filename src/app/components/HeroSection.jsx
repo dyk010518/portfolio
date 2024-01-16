76624be5073e508dce07fda0b0dbd331a10ec897
Daniel <dyk010518@naver.com>
2024-01-16 02:20:06 +0100

    dyk010518 commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -35,7 +35,7 @@ const HeroSection = () => {
                 </p>
                 <div>
                     <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
-                        Github
+                        Github Logo
                     </button>
                     <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
                         <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
