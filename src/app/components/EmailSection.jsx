c543407a9468f015e724d0bc473e8f649cf21768
Daniel <dyk010518@naver.com>
2024-01-21 00:02:09 +0100

    done

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -41,10 +41,10 @@ const EmailSection = () => {
     >
         <div className='absolute h-80 w-80 z-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full blur-lg top-full -left-4 transform -translate-x-1/2 -translate-y-1/2'></div>
         <div className='z-10'>
-            <h5 className='text-xl font-bold text-white my-2'>Let's Connect</h5>
+            <h5 className='text-xl font-bold text-white my-2'>Let&apos;s Connect</h5>
             <p className='text-[#ADB7BE] mb-4 max-w-md'>
                 {" "}
-                I'm currently looking for new opportunities. My inbox is always open.
+                I&apos;m currently looking for new opportunities. My inbox is always open.
                 Send me an email to say hi or anything!
             </p>
             <div className='socials flex flex-row gap-2'>
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
             >
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
                     <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-                        Hello, I'm{" "}
+                        Hello, I&apos;m{" "}
                     </span>
                     <br></br>
                     <TypeAnimation
@@ -37,7 +37,7 @@ const HeroSection = () => {
                     />
                 </h1>
                 <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-                    I am a Class of '24 at MIT studying Computer Science and Finance
+                    I am a Class of &apos;24 at MIT studying Computer Science and Finance
                 </p>
                 <div>
                     <Link 
