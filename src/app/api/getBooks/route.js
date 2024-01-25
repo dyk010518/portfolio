e5519c2677d13e3e663787d1f6ccb4e5cfdd3af8
Daniel <dyk010518@naver.com>
2024-01-25 15:13:44 +0100

    different Mongo import trial

--- src/app/api/getBooks/route.js
+++ src/app/api/getBooks/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
-import { MongoClient } from 'mongodb';
+// import { MongoClient } from 'mongodb';
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
 
 const uri = process.env.MONGODB_URI;
 
