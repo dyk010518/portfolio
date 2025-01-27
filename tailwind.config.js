e52b1b67ff405ff3e8a9b6ded8e5677844de6d56
Daniel <dyk010518@naver.com>
2025-01-27 00:11:55 -0500

    tinyml added

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,8 +36,8 @@ module.exports = {
     },
     colors: {
       ...colors,
-      primary: colors.purple,
-      secondary: colors.pink,
+      primary: colors.teal,
+      secondary: colors.green,
       tertiary: colors.blue,
     },
   },
