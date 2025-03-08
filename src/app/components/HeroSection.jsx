1cc269c399f74c0747b56aa8ee4e2253c3a93bdd
Daniel <dyk010518@naver.com>
2025-03-08 00:03:48 -0500

    feat: Update hero image and size

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -70,8 +70,8 @@ const HeroSection = () => {
                         src="/images/hero-image.png"
                         alt="hero image"
                         className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-                        width={300}
-                        height={300}
+                        width={400}
+                        height={400}
                     />
                 </div>
             </motion.div>
