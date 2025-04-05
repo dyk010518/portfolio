b5d9256ae2c27449629cfbd9e373877cd2d5735f
Daniel Kim <dyk010518@naver.com>
2025-04-04 21:23:48 -0400

    feat: Consolidate filter css with globals.css

--- src/app/components/Checkbox.jsx
+++ src/app/components/Checkbox.jsx
@@ -67,7 +67,7 @@ const Checkbox = ( {options, current, change, defaultDisplay, setFocusedIndex} )
       <div className="relative inline-block" ref={checkboxRef}>
         <button
           type="button"
-          className="px-4 py-2 justify-center text-white border-2 border-primary-500 hover:bg-primary-500 focus:ring-4 focus:outline-none focus:ring-primary-900 font-medium text-xs md:text-sm inline-flex items-center whitespace-nowrap w-40 md:w-48"
+          className="book-filter-btn"
           onClick={toggleCheckbox}
         >
           {displayMessage}
@@ -75,7 +75,7 @@ const Checkbox = ( {options, current, change, defaultDisplay, setFocusedIndex} )
         </button>
 
         {isOpen && (
-          <div className="origin-top-right absolute right-0 mt-2 w-40 md:w-48 rounded-lg  bg-white ring-1 ring-black ring-opacity-5">
+          <div className="book-filter-option">
             <ul role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
               <li key="all">
                 <a
--- src/app/components/DropdownButton.jsx
+++ src/app/components/DropdownButton.jsx
@@ -35,7 +35,7 @@ const DropdownButton = ( {options, current, change, setFocusedIndex} ) => {
       <div className="relative inline-block" ref={dropdownRef}>
         <button
           type="button"
-          className="px-4 py-2 justify-center text-white border-2 border-primary-500 hover:bg-primary-500 focus:ring-4 focus:outline-none focus:ring-primary-900 font-medium text-xs md:text-sm inline-flex items-center whitespace-nowrap w-40 md:w-48"
+          className="book-filter-btn"
           onClick={toggleDropdown}
         >
           {current} 
@@ -43,7 +43,7 @@ const DropdownButton = ( {options, current, change, setFocusedIndex} ) => {
         </button>
 
         {isOpen && (
-          <div className="origin-top-right absolute right-0 mt-2 w-40 md:w-48 rounded-lg  bg-white ring-1 ring-black ring-opacity-5">
+          <div className="book-filter-option">
             <ul role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
               {options.map((option, index) => {
                 return(
