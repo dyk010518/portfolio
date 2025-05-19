60aeeb77fad777480f0d6c20d54ea5f1affbdeb9
Daniel <dyk010518@naver.com>
2025-05-19 17:14:42 -0400

    feat: Change bookshelf page title

--- src/app/bookshelf/page.js
+++ src/app/bookshelf/page.js
@@ -1,16 +1,21 @@
-import Image from 'next/image'
 import Navbar from "../components/Navbar"
 import Footer from '../components/Footer'
 import BookGalley from '../components/BookGallery/BookGallery'
+import Head from 'next/head';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#010f18]">
-      <Navbar />
-      <div className="container mt-24 mx-auto px-8 md:px-12 py-4">
-        <BookGalley />
-      </div>
-      <Footer />
-    </main>
+    <>
+      <Head>
+        <title>Daniel's Bookshelf</title>
+      </Head>
+      <main className="flex min-h-screen flex-col bg-[#010f18]">
+        <Navbar />
+        <div className="container mt-24 mx-auto px-8 md:px-12 py-4">
+          <BookGalley />
+        </div>
+        <Footer />
+      </main>
+    </>
   )
 }
